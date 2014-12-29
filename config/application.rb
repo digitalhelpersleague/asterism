c24f82015bb50c67f554ee82743ca3b8002ffbea
Sergey Besedin <kr3ssh@gmail.com>
2014-12-29 22:18:19 +0300

    no need to initialize to precompile assets need for building docker image

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module Asterism
 
     config.autoload_paths << Rails.root.join('lib')
 
+    config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
